58e6a00075f3ef8a8bcdbb0c9b061507e181c58c
Rahmat Hidayat <email": null>
2017-09-19 11:16:25 +0700

    uglyNUmber selesai

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,35 +1,36 @@
 'use strict'
-
-function maxDivides(number, divisible) {
+//replace maxDivides with maxDivide
+function maxDivide(number, divisible) {
   while(number % divisible == 0) {
     number = number / divisible;
   }
-  return numbers;
+  return number; //removed s
 }
 
 function isUgly(numberUgly) {
   numberUgly = maxDivide(numberUgly, 2);
   numberUgly = maxDivide(numberUgly, 3);
   numberUgly = maxDivide(numberUgly, 5);
-
-  return (numbers = 1) ? true : false;
+  return (numberUgly == 1) ? true : false;
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count //replaced == with =
 
   while(input > countUglyNumber) {
+    debugger;
     i++;
     if(isUgly(i) == 1) {
-      counts++;
+      countUglyNumber++;
     }
-    return i;
+
   }
+  return i
 }
 
 //driver code
 console.log(getUglyNo(150)); //5832
-console.log(getUglyNo(7)); //8
+console.log(getUglyNo(3)); //8
 console.log(getUglyNo(10)); //12
 console.log(getUglyNo(15)); //24
