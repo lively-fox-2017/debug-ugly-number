88c11b4a33bb4490cabee34a6deb5c3db40ff9a3
kalibani <kalibani.ka@gmail.com>
2017-09-19 11:52:50 +0700

    debugging ugly number done

--- uglyNumber.js
+++ uglyNumber.js
@@ -2,32 +2,33 @@
 
 function maxDivides(number, divisible) {
   while(number % divisible == 0) {
-    number = number / divisible;
+    number /= divisible;
   }
-  return numbers;
+  return number;
 }
 
-function isUgly(numberUgly) {
-  numberUgly = maxDivide(numberUgly, 2);
-  numberUgly = maxDivide(numberUgly, 3);
-  numberUgly = maxDivide(numberUgly, 5);
+function isUgly(numberUgly) { // cek satu2, numberUgly sehingga menghasilkan return 1 , cek nya dengan menjalankan function maxDivides
+  numberUgly = maxDivides(numberUgly, 2);
+  numberUgly = maxDivides(numberUgly, 3);
+  numberUgly = maxDivides(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  return (numberUgly = 1) ? true : false; // jika true lanjut cek ke function getUglyNo, jika false cek ke maxDivides yg selanjutnya
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count
 
-  while(input > countUglyNumber) {
-    i++;
-    if(isUgly(i) == 1) {
-      counts++;
+  while(input > countUglyNumber) { // cek input selama masih lebih dari countUglyNumber, jika ya
+    i++; // i naik 1
+    if(isUgly(i) == 1) { // cek ke function isUgly apakah i nya =1, jika ya countUglyNumber nambah 1
+      countUglyNumber++;
     }
-    return i;
   }
+  return i;
 }
 
+// uglyNumber adalah bilangan yang habis dibagi 2, 3, 5
 //driver code
 console.log(getUglyNo(150)); //5832
 console.log(getUglyNo(7)); //8
