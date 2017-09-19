e55da28b24050cb805b89f9b208461c0428d7d2a
chandrabuwana <cbuwana@gmail.com>
2017-09-19 14:56:52 +0700

    ugly number Done

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,35 +1,40 @@
 'use strict'
 
-function maxDivides(number, divisible) {
+function maxDivide(number, divisible) {
   while(number % divisible == 0) {
     number = number / divisible;
   }
-  return numbers;
+  // console.log(number);
+  return number;
 }
 
 function isUgly(numberUgly) {
   numberUgly = maxDivide(numberUgly, 2);
   numberUgly = maxDivide(numberUgly, 3);
   numberUgly = maxDivide(numberUgly, 5);
-
-  return (numbers = 1) ? true : false;
+// console.log(numberUgly);
+  return (numberUgly == 1)? true:false ;
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
-
+  let countUglyNumber = 1; // ugly number count
+let newArr=[]//
   while(input > countUglyNumber) {
     i++;
+    // console.log(input);
     if(isUgly(i) == 1) {
-      counts++;
+      countUglyNumber++;
+      if(newArr<2){//
+        newArr.push(1,i)//
+      }
     }
-    return i;
   }
+  return i
 }
 
 //driver code
-console.log(getUglyNo(150)); //5832
 console.log(getUglyNo(7)); //8
+console.log(getUglyNo(150)); //5832
 console.log(getUglyNo(10)); //12
 console.log(getUglyNo(15)); //24
